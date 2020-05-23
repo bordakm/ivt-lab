b8db8397a7af81e0f104b47ca55a17f722a3de55
bordakm <mate@webtar.hu>
2020-05-23 19:28:17 +0200

    more tests

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -96,4 +96,54 @@ public class GT4500Test {
     assertEquals(true, result);
   }
 
+  @Test
+  public void fireTorpedo_All_BothEmpty_Failure(){
+    when(mockPrimaryStore.fire(1)).thenReturn(false);
+    when(mockSecondaryStore.fire(1)).thenReturn(false);
+    when(mockPrimaryStore.isEmpty()).thenReturn(true);
+    when(mockSecondaryStore.isEmpty()).thenReturn(true);
+
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+    verify(mockPrimaryStore, times(1)).isEmpty();
+    verify(mockSecondaryStore, times(0)).isEmpty();
+    assertEquals(false, result);
+  }
+
+  @Test
+  public void fireLaser_Failure(){   
+    boolean result = ship.fireLaser(FiringMode.ALL);
+    assertEquals(false, result);
+  }
+
+  @Test
+  public void fireTorpedo_Single_Twice_FirstHasOnlyOne_SecondEmpty(){
+    when(mockPrimaryStore.fire(1)).thenReturn(true);
+    when(mockSecondaryStore.fire(1)).thenReturn(false);
+    when(mockPrimaryStore.isEmpty()).thenReturn(false);
+    when(mockSecondaryStore.isEmpty()).thenReturn(true);
+
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    when(mockPrimaryStore.isEmpty()).thenReturn(true);
+    boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+
+    assertEquals(true, result);
+    assertEquals(false, result2);
+  }
+
+  @Test
+  public void fireTorpedo_Single_Twice_SecondEmpty(){
+    when(mockPrimaryStore.fire(1)).thenReturn(true);
+    when(mockSecondaryStore.fire(1)).thenReturn(false);
+    when(mockPrimaryStore.isEmpty()).thenReturn(false);
+    when(mockSecondaryStore.isEmpty()).thenReturn(true);
+
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+    boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+
+    assertEquals(true, result);
+    assertEquals(true, result2);
+  }
+
 }
